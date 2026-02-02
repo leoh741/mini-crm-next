568e8d36f110f1445c2b2e30a8df4b5ab90e39ec
Leonel <leonel@digitalspace.com>
2026-02-01 22:58:40 -0300

    aaaa

--- components/reports/SectionEditor.jsx
+++ components/reports/SectionEditor.jsx
@@ -121,3 +121,5 @@ export default function SectionEditor({ section, onChange, onDelete }) {
   );
 }
 
+
+
--- components/reports/SharePanel.jsx
+++ components/reports/SharePanel.jsx
@@ -138,3 +138,5 @@ export default function SharePanel({ informe, onUpdate }) {
   );
 }
 
+
+
